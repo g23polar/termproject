b2286f600c75eda5e49810f93d5f3b2a0ad60036
g23 <gautamnair023@gmail.com>
2021-08-12 18:47:24 -0400

    some

--- ang/src/app/review/review.component.ts
+++ ang/src/app/review/review.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+// import {PARecord} from "../_models/PARecord";
 
 @Component({
   selector: 'app-review',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./review.component.css']
 })
 export class ReviewComponent implements OnInit {
+  // @Input() parecord: PARecord;
 
   constructor() { }
 
