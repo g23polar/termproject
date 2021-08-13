01cd58207e039d67ea51be03818d4497fae86477
0x7B5 <vladcmunteanu@gmail.com>
2021-08-13 19:37:09 -0400

    Changed toolbar.

--- ang/src/app/app.component.ts
+++ ang/src/app/app.component.ts
@@ -16,6 +16,7 @@ export class AppComponent {
   first = 'G';
   last = 'N';
   initials = 'fmerlkf fekrmlf';
+  public circleColor: string;
 
   constructor(  private router: Router,
                 private authService: AuthService
@@ -23,6 +24,7 @@ export class AppComponent {
     this.authService.currentUser.subscribe(x => {
       // console.log("do i get here");
       this.currentUser = x;
+      this.circleColor = "#707070";
 
     });
 
@@ -63,13 +65,15 @@ export class AppComponent {
       // this.logout();
     }
     // console.log("returning initials " , this.initials);
-    return this.initials
+    return 'AI'
   }
 
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
   }
+
+  
 
 
 
