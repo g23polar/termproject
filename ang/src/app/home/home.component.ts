36553f4a0a9292c2427ccf6e7407ab07229a29ba
0x7B5 <vladcmunteanu@gmail.com>
2021-08-13 19:06:44 -0400

    Changes.

--- ang/src/app/home/home.component.ts
+++ ang/src/app/home/home.component.ts
@@ -1,28 +1,13 @@
-<<<<<<< HEAD
-import {Component, OnDestroy, OnInit, Output} from '@angular/core';
-import {first, mergeMap} from 'rxjs/operators';
-=======
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { first, mergeMap } from "rxjs/operators";
->>>>>>> master
 
 import { NotificationService } from "../_services/notification.service";
 import { UserService } from "../_services/user.service";
 import { Router } from "@angular/router";
 import { User } from "../_models/user";
 
-<<<<<<< HEAD
-import {NotificationService} from '../_services/notification.service';
-import {PARecord} from '../_models/PARecord';
-import {PArecordService} from '../_services/parecord.service';
-import {UserService} from '../_services/user.service';
-import {Router} from "@angular/router";
-import {User} from "../_models/user";
-import {from} from "rxjs";
-=======
 @Component({
   templateUrl: "home.component.html",
->>>>>>> master
 
   styleUrls: ["home.component.css"],
 })
@@ -65,29 +50,12 @@ export class HomeComponent implements OnInit {
   //       this.notifService.showNotif(error); });
   // }
 
-<<<<<<< HEAD
-  deletePARecord(date) {
-    // this.userService.deleteActivity(date);
-    this.parecordservice.delete(date).pipe(first()).subscribe( () => { this.parecords = null;
-                                                                       this.loadAllPArecords();
-    });
-  }
-  // @ts-ignore
-    @Output() searchcriteria = new EventEmitter<String>();
-  // searchword: any;
-  searchword = "testword"
-  searchThis() {
-  // @ts-ignore
-  this.searchcriteria.emit(this.searchword)
-  }
-=======
   // deletePARecord(date) {
   //   // this.userService.deleteActivity(date);
   //   this.parecordservice.delete(date).pipe(first()).subscribe( () => { this.parecords = null;
   //                                                                      this.loadAllPArecords();
   //   });
   // }
->>>>>>> master
 
   navToCreate() {
     this.router.navigate(["/create"]);
