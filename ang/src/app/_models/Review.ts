e3ceaee584bcdd4cea4c9f5b1db9ca588f01ade2
g23 <gautamnair023@gmail.com>
2021-08-12 22:03:19 -0400

    done for the night

---
+++ ang/src/app/_models/Review.ts
@@ -0,0 +1,15 @@
+// TODO
+
+
+import {PAType} from './PAType';
+import {User} from './user';
+
+export class Review {
+
+  description: string;
+  location: string;
+  rating: number;
+  createdDate: Date;
+  createdBy: User;
+}
+
--- ang/src/app/_services/review.service.ts
+++ ang/src/app/_services/review.service.ts
@@ -1,54 +1,49 @@
-// TODO
+// TODO 1/2
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import {AuthService} from './auth.service';
-import {PARecord} from '../_models/PARecord';
+import {Review} from '../_models/Review';
 
 
 
 
 @Injectable({ providedIn: 'root' })
-export class PArecordService {
+export class ReviewService {
   constructor(private http: HttpClient, private auth: AuthService) { }
 
   getAll() {
-    return this.http.get<PARecord[]>(`http://localhost:3030/parecord/getparecords`);
+    return this.http.get<Review[]>(`http://localhost:3030/review/getreview`);
   }
 
 
 
 
 
-  addRandom() {
-    const randparecord = {
-      calories: Math.floor(Math.random() * 2500),
-      minutes: Math.floor(Math.random() * 180),
-      steps:  Math.floor(Math.random() * 25000),
-      activityType: Math.floor(Math.random() * 3)
-    };
+  // addRandom() {
+  //   const randparecord = {
+  //     calories: Math.floor(Math.random() * 2500),
+  //     minutes: Math.floor(Math.random() * 180),
+  //     steps:  Math.floor(Math.random() * 25000),
+  //     activityType: Math.floor(Math.random() * 3)
+  //   };
+  //   return this.http.post(`http://localhost:3030/parecord/addreview`, randparecord);
+  // }
 
-
-    return this.http.post(`http://localhost:3030/parecord/addparecord`, randparecord);
-
-  }
-
-  add(data){
+  add(data: Review) {
     const dataRecord = {
-      calories: data.calories,
-      minutes: data.minutes,
-      // steps:  data.steps,
-      activityType: data.activityType
+      location: data.location,
+      description: data.description,
+      rating: data.rating
     };
 
-    return this.http.post(`http://localhost:3030/parecord/addparecord`, dataRecord);
+    return this.http.post(`http://localhost:3030/review/addreview`, dataRecord);
 
   }
 
   delete(date: string) {
-    return this.http.delete(`http://localhost:3030/parecord/${date}`);
-
+    return this.http.delete(`http://localhost:3030/review/${date}`);
   }
 
 
